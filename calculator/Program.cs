107f4dbcf7f1d651347b212ae72661df8b9c469f
HelenaMiur4 <101336600+lena-my@users.noreply.github.com>
2024-06-07 11:18:07 +0200

    deleted instructions

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,23 +1,3 @@
-﻿
-/* ## Une calculette
-
-Réalise une calculette basique avec cinq fonctions :
-
-* Add qui additionne deux nombres flottants
-* Subtract qui soustrait deux nombres flottants
-* Multiply qui multiplie deux nombres flottants
-* Divide qui divise deux nombres flottants
-* Modulo qui renvoie le reste d'une division de nombres entiers
-
-Fais précéder chacune des fonctions du mot-clé static.
-
-
-## Critères de validation
-
-* Un lien vers ta solution GitHub
-* Une boucle infinie permet à l'utilisateur d'utiliser la calculette plusieurs fois sans relancer le programme
-* Un message d'erreur s'affiche dans le cas où l'utilisateur rentre une mauvaise opération */
-
 using System.Globalization;
 using System.Transactions;
 
